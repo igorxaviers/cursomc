a518662b5ccd8d1f47c0e8bf3d2a777bcbfb0c50
Igor Xavier <igorxavier.ixds@gmail.com>
2021-09-07 19:59:50 -0300

    Projeto criado

---
+++ src/test/java/com/xavinho/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavinho.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

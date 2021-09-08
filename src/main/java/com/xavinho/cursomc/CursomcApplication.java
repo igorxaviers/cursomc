b269daed4ce7ef4cac10fc28629050ce0c6f6c04
Igor Xavier <igorxavier.ixds@gmail.com>
2021-09-07 23:01:33 -0300

    Criando operação de instanciação

--- src/main/java/com/xavinho/cursomc/CursomcApplication.java
+++ src/main/java/com/xavinho/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.xavinho.cursomc;
 
+import com.xavinho.cursomc.domain.Categoria;
+import com.xavinho.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    CategoriaRepository catRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        catRepository.saveAll(Arrays.asList(cat1,cat2));
+    }
 }
